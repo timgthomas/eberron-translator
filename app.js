22f55d4705d11da470c6bc6da14aee3305c02c96
Tim G. Thomas <tim@timgthomas.com>
2019-06-13 22:59:25 -0500

    display previous results

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+let previousResults = []
 document.addEventListener('DOMContentLoaded', () => {
   document.querySelector('form').addEventListener('submit', evt => {
     evt.preventDefault()
@@ -6,9 +7,13 @@ document.addEventListener('DOMContentLoaded', () => {
     let output
     try {
       output = window[language](input)
+      if (previousResults.slice(-1)[0] !== output) {
+        previousResults.push(output)
+      }
     } catch (ex) {
       output = '(Error)'
     }
     document.querySelector('#result').value = output
+    document.querySelector('#results').innerHTML = previousResults.slice(-5).reverse().map(x => '<li>' + x + '</li>').join('')
   })
 })
